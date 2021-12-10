7c9af5299bc49453b213102db2083c2537369470
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-10 10:18:16 +0200

    get_next_line previous day's work

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,72 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 19:21:02 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/06 19:21:15 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/08 19:38:54 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include "libft/includes/libft.h"
+
+static int	free_buffer_lst(t_list *head)
+{
+	t_list		*next;
+
+	while (head)
+	{
+		free(((t_buffer *)head->content)->buf);
+		next = head->next;
+		free(head);
+		head = next;
+	}
+	return (-1);
+}
+
+/*
+	Negative file descriptor fd will remove node, positive one will add.
+*/
+
+static char	*ft_lstfetch(t_list *head, int fd)
+{
+	t_buffer	*buf;
+
+	while (head)
+	{
+		if (((t_buffer *)head->content)->fd == fd)
+			return (((t_buffer *)head->content)->buf);
+		head = head->next;
+	}
+	buf = (t_buffer *)malloc(sizeof(t_buffer));
+	if (!buf)
+		return (NULL);
+	buf->fd = fd;
+	buf->buf = NULL;
+	head->next = ft_lstnew(buf, sizeof(t_buffer));
+}
+
 int	get_next_line(const int fd, char **line)
 {
-	
+	static t_list	*fd_lst;
+	t_buffer		*buf;
+	char			stack_buf[BUFF_SIZE];
+	ssize_t			read_bytes;
+	char			*end_of_line;
+
+	buf = ft_lstfetch(fd_lst, fd);
+	if (!buf)
+		return (free_buffer_lst(fd_lst));
+	while (1)
+	{
+		read_bytes = read(buf->fd, stack_buf, BUFF_SIZE);
+		if (read_bytes < 0)
+			return (-1);
+		buf->buf = (char *)ft_memdup(stack_buf, read_bytes);
+		if (!buf->buf)
+			return (free_buffer_lst(fd_lst));
+		end_of_line = (char *)ft_memchr(buf, '\n', read_bytes);
+		if (end_of_line)
+		{
+			
+		}
+	}
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,20 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 19:23:04 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/06 19:26:12 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/08 18:19:29 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 32
+
+int	get_next_line(const int fd, char **line);
+
+typedef struct s_buffer
+{
+	char	*buf;
+	int		fd;
+}	t_buffer;
+
 #endif
