32153a44454666a37c0e4457df47eeabbc5413d3
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-12-14 18:40:17 +0200

    3rd iteration, great success

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 19:21:02 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/14 18:26:57 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/14 18:36:53 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,19 @@ int	get_next_line(const int fd, char **line)
 	ssize_t		bytes;
 	char		*end_of_line;
 	char		*temp;
+	char		first;
 
 	*line = ft_strnew(0);
 	end_of_line = NULL;
+	bytes = 0;
+	first = 0;
 	if (!buf)
 	{
+		first = 1;
 		buf = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
 		if (!buf)
 			return (-1);
+		*buf = '\0';
 	}
 	else
 		end_of_line = ft_strchr(buf, '\n');
@@ -37,13 +42,13 @@ int	get_next_line(const int fd, char **line)
 		temp = *line;
 		*line = ft_strjoin(*line, buf);
 		free(temp);
+		if (!first && !end_of_line)
+			buf = ft_strchr(buf, '\0') - BUFF_SIZE;
 		bytes = read(fd, buf, BUFF_SIZE);
 		if (bytes <= 0)
 			return ((int)bytes);
 		buf[bytes] = '\0';
 		end_of_line = ft_strchr(buf, '\n');
-		/*if (!end_of_line)
-			buf = ft_strchr(buf, '\0') - bytes;*/
 	}
 	*end_of_line = '\0';
 	temp = *line;
