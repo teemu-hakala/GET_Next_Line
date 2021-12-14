3bcfb9ba6d599f9c72aac0310930b49e9efd9a5a
Teemu Hakala <thakala@c1r6p16.hive.fi>
2021-12-14 17:06:31 +0200

    template for one fd

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 19:21:02 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/14 16:36:10 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/14 17:05:43 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,28 @@
 
 int	get_next_line(const int fd, char **line)
 {
-	
+	static char	*head;
+	char		*end_of_line;
+	char		*temp;
+	ssize_t		bytes;
+
+	end_of_line = NULL;
+	if (head)
+		end_of_line = ft_strchr(head, '\n');
+	else
+		head = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
+	if (!head)
+		return (-1);
+	while (!end_of_line)
+	{
+		*line = ft_strjoin(*line, head);
+		bytes = read(fd, head, BUFF_SIZE);
+		if (bytes <= 0)
+			return (bytes);
+		head[bytes] = '\0';
+		temp = *line;
+		end_of_line = ft_strchr(head, '\n');
+		*line = ft_strjoin(*line, head);
+		ft_strdel(temp);
+	}
 }
