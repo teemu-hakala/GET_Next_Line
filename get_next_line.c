d85110328a6a065b552bf851fd6d98fe6fcd9762
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-16 21:48:50 +0200

    all done without norminette though

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 19:21:02 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/16 21:20:48 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/16 21:47:16 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,59 +17,65 @@
 
 int	get_next_line(const int fd, char **line)
 {
-	static char	*buf;
-	static char	*debug;
+	static char	*buf[FD_MAX + 1];
+	static char	**debug;
 	char		*end_of_line;
 	char		*temp;
 	ssize_t		bytes;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !line || fd > FD_MAX)
 		return (-1);
 	*line = ft_strnew(0);
 	end_of_line = NULL;
-	if (!buf)
+	if (!buf[fd])
 	{
-		buf = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
-		if (!buf)
+		buf[fd] = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
+		if (!buf[fd])
 			return (-1);
-		*buf = '\0';
-		*(buf + BUFF_SIZE) = '\0';
+		*(buf[fd]) = '\0';
+		*(buf[fd] + BUFF_SIZE) = '\0';
 		debug = buf;
 	}
 	else
-		end_of_line = ft_strchr(buf, '\n');
+		end_of_line = ft_strchr(buf[fd], '\n');
 	while (!end_of_line)
 	{
 		temp = *line;
-		*line = ft_strjoin(*line, buf);
+		*line = ft_strjoin(*line, buf[fd]);
 		free(temp);
 		if (!*line) //err = -!*line;
 			return (-1);
-		bytes = read(fd, buf, BUFF_SIZE);
+		temp = buf[fd];
+		buf[fd] = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
+		if (!buf[fd])
+			return (-1);
+		//buf[fd][BUFF_SIZE] = '\0';
+		free(temp);
+		bytes = read(fd, buf[fd], BUFF_SIZE);
 		if (bytes < 0)
 			return (-1); //err = bytes; (man 2 read) -> -1
 		if (!bytes)
 		{
 			if (ft_strlen(*line))
 			{
-				*buf = '\0';
+				*(buf[fd]) = '\0';
 				return (1);
 			}
-			free(buf);
-			buf = NULL;
+			free(buf[fd]);
+			buf[fd] = NULL;
 			return (0); // bytes <= 0; return (bytes);
 		}
-		buf[bytes] = '\0';
-		end_of_line = ft_strchr(buf, '\n');
+		buf[fd][bytes] = '\0';
+		end_of_line = ft_strchr(buf[fd], '\n');
 	}
 	*end_of_line = '\0';
 	temp = *line;
-	*line = ft_strjoin(*line, buf);
+	*line = ft_strjoin(*line, buf[fd]);
 	free(temp);
-	temp = buf;
-	buf = ft_strdup(end_of_line + 1);
+	temp = buf[fd];
+	buf[fd] = ft_strdup(end_of_line + 1);
 	free(temp);
-	if (!buf)
+	if (!buf[fd])
 		return (-1);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 19:23:04 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/14 17:42:11 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/16 21:25:35 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 32
+# define FD_MAX 8192
 # include <string.h>
 
 int	get_next_line(const int fd, char **line);
