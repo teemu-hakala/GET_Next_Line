c0cd11cb9dd1577ac1f14395fe710e20c2f1c32d
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-17 16:20:55 +0200

    greater maximum fd

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 19:23:04 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/16 21:25:35 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/17 16:09:12 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
-# define FD_MAX 8192
+# define BUFF_SIZE 2797
+# define FD_MAX 10240
 # include <string.h>
 
 int	get_next_line(const int fd, char **line);
