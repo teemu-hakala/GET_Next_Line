3e03891d6a6f277a70c6bb9527076c57c61e3f39
Teemu Hakala <thakala@c1r6p14.hive.fi>
2021-12-19 15:41:50 +0200

    second attempt (mistakes in handling the last line)

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 19:21:02 by thakala           #+#    #+#             */
-/*   Updated: 2021/12/19 14:23:49 by thakala          ###   ########.fr       */
+/*   Updated: 2021/12/19 15:33:50 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,21 @@ static int	ft_initialize_buffer(t_string **buf)
 	return (-!(*buf)->string);
 }
 
-static int	ft_strjoinfree(char **line, char **string)
+static int	ft_strjoinfree(char **line, char *string)
 {
 	char	*del;
 
 	del = *line;
-	*line = ft_strjoin(*line, *string);
+	*line = ft_strjoin(*line, string);
 	free(del);
 	return (-!*line);
 }
 
 static int	ft_handle_tail(char **line, t_string **buf, char *end_of_line)
 {
-	char	*del;
-
 	*end_of_line = '\0';
-	ft_strjoinfree(line, (*buf)->string);
-	(*buf)->index = end_of_line - (*buf)->string;
+	ft_strjoinfree(line, &(*buf)->string[(*buf)->index]);
+	(*buf)->index = end_of_line - (*buf)->string + 1;
 	if (!*(end_of_line + 1))
 	{
 		(*buf)->index = 0;
@@ -55,17 +53,24 @@ static int	ft_fill(char **line, t_string **buf, int fd, char *end_of_line)
 
 	while (!end_of_line)
 	{
-		if (ft_strjoinfree(line, &(*buf)->string))
+		if (ft_strjoinfree(line, &(*buf)->string[(*buf)->index]))
 			return (-1);
-		(*buf)->string -= (*buf)->index;
-		bytes = read(fd, *buf, BUFF_SIZE); //store into ->read (binary)
+		bytes = read(fd, (*buf)->string, BUFF_SIZE); //into ->read (binary)
 		if (bytes < 0)
 			return (-1);
 		if (!bytes)
-			return (!!(*buf)->index);
+		{
+			if (!(*buf)->index)
+			{
+				ft_strdel(&(*buf)->string);
+				ft_memdel((void **)buf);
+			}
+			return (!!*buf);
+		}
 		else if (bytes < BUFF_SIZE)
 			(*buf)->string[bytes] = '\0';
-		end_of_line = ft_strchr((*buf)->string, '\n');
+		(*buf)->index = 0;
+		end_of_line = ft_strchr(&(*buf)->string[(*buf)->index], '\n');
 	}
 	return (ft_handle_tail(line, buf, end_of_line));
 }
@@ -78,9 +83,14 @@ int	get_next_line(const int fd, char **line)
 	if (fd < 0 || !line || fd > FD_MAX)
 		return (-1);
 	*line = ft_strnew(0);
-	if (!buf[fd] && ft_initialize_buffer(&buf[fd]) == -1)
-		return (-1);
-	result = ft_fill(line, &buf[fd], fd, ft_strchr(buf[fd]->string, '\n'));
+	if (!buf[fd])
+	{
+		buf[fd] = (t_string *)malloc(sizeof(t_string));
+		if (!buf[fd] || ft_initialize_buffer(&buf[fd]) == -1)
+			return (-1);
+	}
+	result = ft_fill(line, &buf[fd], fd, \
+		ft_strchr(&buf[fd]->string[buf[fd]->index], '\n'));
 	if (result == -1)
 		ft_strdel(line);
 	return (result);
